e4ef02f0c50364f15d261769ef06cfe6f33fbe27
aradhana Arora <aradhana_arora2001@yahoo.com>
2015-06-18 12:09:24 +0530

    modified

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,10 @@
-activity_names <- read.table("./activity_labels.txt")
-
+# Assuming we have "UCI HAR Dataset" directory in working directory.
 #Load dplyr package
 library(dplyr)
 library(plyr)
-setwd("UCI HAR Dataset") 
+setwd("UCI HAR Dataset")
+#load activity labels
+activity_names <- read.table("./activity_labels.txt")
 # get the test data
 test_data <- read.table("./test/X_test.txt")
 activity_labels_test <- read.table("./test/y_test.txt")
@@ -26,12 +27,17 @@ col_names<- read.table("./features.txt")
 names(merge_data) <- c(as.character(col_names[,2]), "activity", "subject")
 #removing duplicate names
 merge_data_nodup <-merge_data[, !duplicated(colnames(merge_data))]
-
+#getting only variables related to mean and standard deviation
 mean_std_cols <- select(merge_data_nodup, matches("(mean\\(\\)|std\\(\\)|subject|activity)"))
-mean_std_cols_labels <- mutate(mean_std_cols,activity_labels=activity_names[,2][activity])
-tmp <- select(mean_std_cols_labels,-activity)
-#names <- sub("...", ".",make.names(colnames(result)), fixed=TRUE)
 
-#names(tmp) <-  sub("..",".", names,fixed=TRUE)
+#applying activity labels to IDs
+mean_std_cols_labels <- mutate(mean_std_cols,activity_labels=activity_names[,2][activity])
+
+#giving valid names to columns
+tmp <- select(mean_std_cols_labels,-activity)
+names <- sub("...", ".",make.names(colnames(tmp)), fixed=TRUE)
+names(tmp) <-  sub("..",".", names,fixed=TRUE)
+# calculate the average for all the variables for each subject and activity.
 result <- ddply(tmp, c("activity_labels", "subject"), numcolwise(mean))
-write.table(result, file = "Submit.txt", col.names=TRUE)
+# generate the tidy result set in a file.
+write.table(result, file = "tidyData.txt", col.names=TRUE, row.name=FALSE, quote=FALSE)
